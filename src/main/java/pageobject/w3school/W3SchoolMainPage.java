064c402f0ddb61fd66e834161c7bedf250d1b21c
nataliamurzahanova <ndomanska@gmail.com>
2019-12-22 22:38:58 +0200

    commit

--- src/main/java/pageobject/w3school/W3SchoolMainPage.java
+++ src/main/java/pageobject/w3school/W3SchoolMainPage.java
@@ -2,7 +2,9 @@ package pageobject.w3school;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageobject.AbstractPage;
 
 public class W3SchoolMainPage extends AbstractPage {
@@ -10,16 +12,22 @@ public class W3SchoolMainPage extends AbstractPage {
     @FindBy(xpath = "//iframe[@src='default.asp']")
     private WebElement iframe;
 
-    @FindBy(xpath = "//a[@title='CSS Tutorial']")
+    @FindBy(xpath = "//a[@title='JavaScript Tutorial']")
     private WebElement menuItem;
+
+    @FindBy(xpath = "//a[@title='Home']")
+    private WebElement homeButton;
 
     public W3SchoolMainPage(WebDriver webDriver){
         super(webDriver);
     }
 
     public void selectMenuItem(){
+        Actions actions = new Actions(webDriver);
         webDriver.switchTo().frame(iframe);
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(menuItem));
         menuItem.click();
+        actions.moveToElement(homeButton).build().perform();
     }
 
     public String getSelectedMenuItemColor(){
