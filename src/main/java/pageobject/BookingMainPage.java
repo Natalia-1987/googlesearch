110c91b5e2b7b07fe9fb6cac140976878150abdd
nataliamurzahanova <ndomanska@gmail.com>
2019-12-22 22:02:22 +0200

    commit

--- src/main/java/pageobject/BookingMainPage.java
+++ src/main/java/pageobject/BookingMainPage.java
@@ -3,6 +3,7 @@ package pageobject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageobject.panels.BookingCalendarPanel;
 
@@ -20,6 +21,9 @@ public class BookingMainPage extends AbstractPage{
     @FindBy (xpath = "//button[contains(@class, 'sb-searchbox__button')]")
     private WebElement searchButton;
 
+    @FindBys({
+            @FindBy(xpath = "//span[@class='search_h1_name']")
+    })
     private List<WebElement> predictiveSearchList;
 
 
