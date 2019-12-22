a08acbe57a5f70e901ff6c4f8dbd30eb5adc88f3
nataliamurzahanova <ndomanska@gmail.com>
2019-12-22 23:32:13 +0200

    commit

--- src/test/java/HotlineTests.java
+++ src/test/java/HotlineTests.java
@@ -1,17 +1,25 @@
 import common.model.HotlineItem;
+import helper.PropertiesHelper;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.unitils.reflectionassert.ReflectionAssert;
 import pageobject.HotlinePage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HotlineTests extends BaseTest {
 
     @Test
-    public void verifyHotlineAscendingSortingTest(){
+    public void verifyHotlineAscendingSortingTest() throws IOException {
+        PropertiesHelper propertiesHelper = new PropertiesHelper();
+
         WebDriver webDriver = getWebDriver();
-        webDriver.get("https://hotline.ua/mobile/chehly-sumki-futlyary-dlya-mobilnyh-i-smartfonov/71061-380639/");
+
+        String url = "https://hotline.ua/mobile/chehly-sumki-futlyary-dlya-mobilnyh-i-smartfonov/71061-380639/";
+
+        webDriver.get(url);
+
         HotlinePage hotlinePage = new HotlinePage(webDriver);
 
         List<HotlineItem> unsortedHotlineItemsList = hotlinePage.getHotlineProducts();
