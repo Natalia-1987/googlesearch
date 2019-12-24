adfe9e9a7432ddbcafa3e47b752c1cbd18eb634d
nataliamurzahanova <ndomanska@gmail.com>
2019-12-24 21:45:18 +0200

    commit

--- src/test/java/HotlineTests.java
+++ src/test/java/HotlineTests.java
@@ -1,5 +1,6 @@
 import common.model.HotlineItem;
 import helper.PropertiesHelper;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.unitils.reflectionassert.ReflectionAssert;
@@ -8,17 +9,27 @@ import pageobject.HotlinePage;
 import java.io.IOException;
 import java.util.List;
 
+
 public class HotlineTests extends BaseTest {
+
+    private Logger logger = Logger.getLogger(HotlineTests.class);
 
     @Test
     public void verifyHotlineAscendingSortingTest() throws IOException {
+
+        logger.info("Start 'Verify Hotline Ascending Test'");
+
         PropertiesHelper propertiesHelper = new PropertiesHelper();
 
         WebDriver webDriver = getWebDriver();
 
+        logger.info("Init 'Webdriver'");
+
         String url = "https://hotline.ua/mobile/chehly-sumki-futlyary-dlya-mobilnyh-i-smartfonov/71061-380639/";
 
         webDriver.get(url);
+
+        logger.info(String.format("Opened following url '%s'", url));
 
         HotlinePage hotlinePage = new HotlinePage(webDriver);
 
